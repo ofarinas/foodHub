1e06ba492b93b8aee8790ccec4768d918537e7b2
Osvaldo <ofarinas12@gmail.com>
2016-04-12 00:15:24 +0100

    add plugin  $cordova camare

--- www/controller/controllerCreateRecipe.js
+++ www/controller/controllerCreateRecipe.js
@@ -3,10 +3,33 @@
  */
 var listaRecetas = [];
 var cont = 1;
-main.controller('controllerCreateRecipe', function ($scope, factoryRecipeList, $localstorage, $window, $state, $ionicViewService, factoryRecipeLocalStorage) {
+main.controller('controllerCreateRecipe', function ($scope, factoryRecipeList, $localstorage, $window, $state, $ionicViewService, factoryRecipeLocalStorage,$cordovaCamera) {
+  $scope.pictureUrl="";
+  //$scope.mensaje=function(){alert("hola.")};
   $scope.recipe = {};
   $scope.recipe.difficulty=2;
   $scope.listIngredient = [];
+  $scope.mensaje=function(){
+    var options = {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      //allowEdit: true,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 100,
+      targetHeight: 100,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: true,
+      correctOrientation:true
+    };
+    $cordovaCamera.getPicture(options).then(
+      function(data){
+        $scope.pictureUrl="data:image/jpeg;base64,"+data;
+      },function(error){
+        console.log("error");
+      }
+    );
+  };
   $scope.addIngredient = function (ingredientes) {
     $scope.listIngredient.push(angular.copy(ingredientes));
     $scope.recipe.ingredientes = "";
