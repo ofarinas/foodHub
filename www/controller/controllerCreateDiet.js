0450f94556e8654362e4c270e1ca785ddb15d173
Osvaldo <ofarinas12@gmail.com>
2016-04-18 12:39:36 +0100

    starting to make create diet

---
+++ www/controller/controllerCreateDiet.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Osvaldo on 3/21/2016.
+ */
+var listDiet = [];
+var cont = 1;
+main.controller('controllerCreateDiet', function ($scope, factoryRecipeList, $localstorage, $window, $state, $ionicViewService, factoryRecipeLocalStorage,$cordovaCamera) {
+  $scope.diet = {};
+  $scope.diet.difficulty=2;
+  $scope.diet.listRecipe=factoryRecipeList.getRecipe();
+  $scope.diet.listSelectingRecipeName = [];
+  $scope.diet.listSelectingRecipeId = [];
+  $scope.addRecipe = function () {
+    $scope.diet.listSelectingRecipeName.push(angular.copy($scope.diet.recipe.title));
+    $scope.diet.listSelectingRecipeId.push(angular.copy($scope.diet.recipe.id));
+  };
+  $scope.removeIngredient = function (index) {
+    delete $scope.diet.listSelectingRecipeName.splice(index, 1);
+  };
+  $scope.save = function (diet) {
+    if (cont == 1)
+      listDiet = $localstorage.getObject("listDiet");
+    if (listDiet == null) {
+      listDiet = [];
+    }
+    listDiet.push({
+      id: cont++,
+      title: diet.nombre,
+      recetas: $scope.diet.listSelectingRecipeId,
+      description: diet.descripcion,
+      difficulty: diet.difficulty,
+    });
+
+    $localstorage.setObject("listDiet", listDiet);
+    cleanCreateRecipe($scope);
+    $ionicViewService.nextViewOptions({disableBack: true});
+  };
+});
+function cleanCreateRecipe($scope) {
+  $scope.diet.descripcion = "";
+  $scope.diet.nombre = "";
+  $scope.diet.listSelectingRecipeName = [];
+  $scope.diet.listSelectingRecipeId = [];
+}
